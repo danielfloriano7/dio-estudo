ae2798c221939fd1dc5e0e18da18172441361b09
Daniel Floriano Santos <danielfloriano808@gmail.com>
2022-07-19 15:55:19 -0300

    atualizacao

---
+++ Primeiros passos para desenvolvimento web/Projetos/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando o PHP</title>
+</head>
+<body>
+    <?php 
+        for ($i = 1; $i <= 10; $i++) {
+            print("Linha numero " . $i . "<br/>");
+        }
+    ?>
+</body>
+</html>
